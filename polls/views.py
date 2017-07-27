67ef68e9f0872a64e10b9e02361d26903246b093
huangjinyang <huangjinyang@gzac.org>
2017-07-27 21:04:21 +0800

    python

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.http import Http404
+from django.shortcuts import render
+
+from .models import Question
 
 
 def index(request):
@@ -7,9 +11,18 @@ def index(request):
     context = {}
     return HttpResponse(template.render(context, request))
 
+
 def login(request):
     username = request.GET.get('username')
     password = request.GET.get('password')
     if username == '逗比':
         return HttpResponse('恭喜逗比，登录成功！')
     return HttpResponse('登录失败！')
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
